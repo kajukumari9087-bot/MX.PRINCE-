3cb960933d9cde9dc152543500860fb319903be0
rajputbarun489-source <rajputbarun489@gmail.com>
2026-08-01 11:49:58 +0530

    Update 0_SHAAN-FRAME-1.js

--- models/commands/0_SHAAN-FRAME-1.js
+++ models/commands/0_SHAAN-FRAME-1.js
@@ -3,7 +3,7 @@ module.exports.config = {
     version: "7.3.1",
     hasPermssion: 0,
     credits: "PRINCE BABU", 
-    description: "THIS BOT WAS MADE BY MR SHAAN BABU",
+    description: "THIS BOT WAS MADE BY MR PRINCE BABU",
     commandCategory: "PROFILE DP FRAME",
     usages: "PREFIX MENTIONS",
     cooldowns: 5,
