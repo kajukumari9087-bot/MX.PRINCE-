f8d71f67e54843905dbca46fbeef42f139bc8172
rajputbarun489-source <rajputbarun489@gmail.com>
2026-08-01 11:13:08 +0530

    Update #Shaan.js

--- models/commands/#Shaan.js
+++ models/commands/#Shaan.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "SHAAN",
     version: "2.1.1",
   hasPermssion: 0,
-  credits: "𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍", 
+  credits: "MR PRINCE ", 
   description: "Just Respond",
   commandCategory: "no prefix",
     cooldowns: 5, 
