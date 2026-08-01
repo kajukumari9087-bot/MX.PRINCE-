d4ce4e87f33df782c020bd56312d3df9eee51202
rajputbarun489-source <rajputbarun489@gmail.com>
2026-08-01 12:08:52 +0530

    Update 0_SHAAN-HOT-VIDEO.js

--- models/commands/0_SHAAN-HOT-VIDEO.js
+++ models/commands/0_SHAAN-HOT-VIDEO.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "Hotvideos",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "SHAAN",
+  credits: "PRINCE",
   description: "HOT VEDIO",
   commandCategory: "system",
   usages: "Hotvideos",
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["『٭』 💐𝐒𝐇𝐀𝐀𝐍💐 『٭』-HOT-VIDOS"];
+   var hi = ["『٭』 💐PRINCE💐 『٭』-HOT-VIDOS"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
    "https://drive.google.com/uc?id=1a7XsNXizFTTlSD_gRQwK4bDA3HPam56W",
